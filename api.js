f7a5a9e1a336370f065f38f757ad5001e9dc4e8d
im-samwong <98343790+im-samwong@users.noreply.github.com>
2023-02-05 14:20:09 -0500

    #44: Implemented JWT access tokens for increased safety.

--- api.js
+++ api.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app = express();
+const jwt = require('jsonwebtoken');
+
+const secretKey = 'soen341courseterm4';
 
 // Connect to MongoDB database
 const { MongoClient, ServerApiVersion } = require('mongodb');
@@ -14,16 +17,43 @@ console.log('Before connect');
 
     const collection = client.db("SOEN341DB").collection("users");
 
-    // perform actions on the collection object
     // Set up a route to retrieve data from the users collection
     app.get('/users', async (req, res) => {
+        // Verify the token
+        const token = req.header('Authorization').replace('Bearer ', '');
         try {
-          const usersData = await collection.find().toArray();
-          res.send(usersData);
+            const decoded = jwt.verify(token, secretKey);
+            console.log('Decoded:', decoded);
         } catch (error) {
-          console.error(error);
-          res.status(500).send({ message: 'An error occurred' });
+            console.error(error);
+            return res.status(401).send({ message: 'Invalid token' });
         }
+
+        try {
+            const usersData = await collection.find().toArray();
+            res.send(usersData);
+        } catch (error) {
+            console.error(error);
+            res.status(500).send({ message: 'An error occurred' });
+        }
+    });
+
+    // Add a route for token generation
+    app.post('/login', async (req, res) => {
+        // Retrieve the user's credentials from the request body
+        const { username, password } = req.body;
+
+        // Check if the user exists in the database
+        const user = await collection.findOne({ username, password });
+        if (!user) {
+            return res.status(401).send({ message: 'Incorrect username or password' });
+        }
+
+        // Generate a JWT
+        const accessToken = jwt.sign({ username }, secretKey, { expiresIn: '2h' });
+
+        // Send the token to the client
+        res.send({ accessToken });
     });
 
     // Turning on express app to listen at that port. Specify IPv4 for listening.
@@ -36,6 +66,7 @@ console.log('Before connect');
   }
 })();
 console.log('After connect');
+
 
 
 
