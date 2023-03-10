c2743daaa1469b06b4fbacee831ca71013460071
im-samwong <98343790+im-samwong@users.noreply.github.com>
2023-03-10 13:15:46 -0500

    Added authorization header.

--- api.js
+++ api.js
@@ -22,7 +22,7 @@ app.use((req, response, next) => {
     );
     response.setHeader(
         "Access-Control-Allow-Headers",
-        "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
+        "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization"
     );
     next();
 });
