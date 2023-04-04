201869097cca70981ff3da2e22e73aad3ffd8add
Jessica Beauchemin <123212907+JBeauche@users.noreply.github.com>
2023-04-04 15:44:55 -0400

    Update JobList.jsx

--- career-service-webapp/src/adminPage/JobList.jsx
+++ career-service-webapp/src/adminPage/JobList.jsx
@@ -9,6 +9,11 @@ import axios from "axios";
 export default class JobList extends React.Component {
   constructor(props) {
     super(props);
+    const date = new Date();
+    let year = date.getFullYear();
+    let day = date.getDate();
+    let month = date.getMonth() + 1;
+    let currentDate = `${year}-${month}-${day}`;
     this.state = {
       selectedJob: null,
       jobList: [{ title: "N/A" }],
